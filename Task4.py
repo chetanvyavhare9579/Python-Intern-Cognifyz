846977531093898f80c3e1944d34183bca332c03
Chetan Vyavhare <chetanvyavhare410@gmail.com>
2025-10-31 18:02:42 +0530

    Calculator

---
+++ Task4.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /, %): ")
+num2 = float(input("Enter second number: "))
+# Performing operations
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+elif operator == '%':
+    result = num1 % num2
+else:
+    result = "Invalid operator!"
+print("Result:", result)
